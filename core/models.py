0a988159daea0d4a184dfda0bb92390788af5958
keerthivasan <keerthivasanm20@gmail.com>
2021-06-07 19:01:13 +0530

    save

--- core/models.py
+++ core/models.py
@@ -12,7 +12,7 @@ class HallDetails(models.Model):
 class SpotifyToken(models.Model):
        user=models.CharField(max_length=50,unique=True)
        created_at=models.DateTimeField(auto_now_add=True)
-       refresh_token=models.CharField(max_length=150)
-       access_token=models.CharField(max_length=150)
+       refresh_token=models.CharField(max_length=255)
+       access_token=models.CharField(max_length=255)
        expires_in=models.DateTimeField()
       
