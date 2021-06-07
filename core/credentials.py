cc6a4ba6a3318da7158247dcee8d16fd7bd1c925
keerthivasan <keerthivasanm20@gmail.com>
2021-06-07 18:16:59 +0530

    save

--- core/credentials.py
+++ core/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "c44e2477a2ed455e81e22c1f6488cc7b"
 CLIENT_SECRET ="a0478642b706486b9da49631a821e234"
-REDIRECT_URI="http://musicqhall.herokuapp.com/redirect/"
+REDIRECT_URI="https://musiqhall.herokuapp.com/redirect/"
